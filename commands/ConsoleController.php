08ea978bc5a9e3fee136187ef735c1635646f7a7
Суворов Дмитрий Александрович <cr-ovr7@efko.ru>
2023-10-27 03:04:17 +0300

    Add some improvements

--- commands/ConsoleController.php
+++ commands/ConsoleController.php
@@ -1,17 +1,22 @@
-<?php 
+<?php
 
 namespace app\commands;
 
+use Yii;
 use yii\console\Controller;
 use yii\console\ExitCode;
 
 class ConsoleController extends Controller
 {
+	public $month;
+    public $type;
+    public $tonnage;
+	 
 	public function actionCalc()
 	{
 		$counter = 0;
 		$basePath = __DIR__ . '/../runtime/queue.job';
-		while(true) {
+		while (true) {
 			echo "Текущая операция: {$counter}" . PHP_EOL;
 
 			if (file_exists($basePath) === true) {
@@ -26,4 +31,65 @@ class ConsoleController extends Controller
 
 		return ExitCode::OK;
 	}
+
+	public function options($actionID)
+    {
+        return ['month', 'type', 'tonnage'];
+    }
+
+    public function optionAliases()
+    {
+        return [
+            'month' => 'm',
+            'type' => 't',
+            'tonnage' => 'tn',
+        ];
+    }
+	 public function actionCost()
+    {
+        $prices = require(Yii::getAlias('@app/config/prices.php'));
+
+        $priceTable = $prices[$this->type][$this->month];
+
+			$totalCost = null;
+			foreach ($priceTable as $price) {
+				if ($price['тоннаж'] == $this->tonnage) {
+					$totalCost = $price['price'];
+					break;
+				}
+			}
+
+        if (empty($this->month) || empty($this->type) || empty($this->tonnage)) {
+            echo "Ошибка: Все опции обязательны к заполнению.\n";
+            return ExitCode::USAGE;
+        }
+
+        if (!in_array($this->month, ['Январь', 'Февраль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь'])) {
+            echo "Выполнение команды завершено с ошибкой" . PHP_EOL . "некорректное значение месяца.";
+            return ExitCode::USAGE;
+        }
+
+        if (!in_array($this->type, ['Шрот', 'Жмых', 'Соя'])) {
+            echo "Выполнение команды завершено с ошибкой" . PHP_EOL . "некорректное значение типа.";
+            return ExitCode::USAGE;
+        }
+
+        if (!is_numeric($this->tonnage)) {
+            echo "Выполнение команды завершено с ошибкой" . PHP_EOL . "значение тоннажа должно быть числом.";
+            return ExitCode::USAGE;
+        }
+
+        if (!isset($totalCost)) {
+            echo "Выполнение команды завершено с ошибкой" . PHP_EOL . "не найден прайс для значения '--tonnage={$this->tonnage}" . PHP_EOL . "проверьте корректность введенных значений";
+            return ExitCode::USAGE;
+        }
+
+        echo "введенные параметры:\n";
+        echo "месяц: {$this->month}\n";
+        echo "тип: {$this->type}\n";
+        echo "тоннаж: {$this->tonnage}\n";
+		  echo "результат: {$totalCost}\n";
+
+        return ExitCode::OK;
+    }
 }
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -22,63 +22,72 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
+
 <head>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+	<title>
+		<?= Html::encode($this->title) ?>
+	</title>
+	<?php $this->head() ?>
 </head>
+
 <body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+	<?php $this->beginBody() ?>
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>',
-				['label' => 'Calculator', 'url' => ['/site/calculator']]		  
-        ]
-    ]);
-    NavBar::end();
-    ?>
-</header>
+	<header id="header">
+		<?php
+		NavBar::begin([
+			'brandLabel' => Yii::$app->name,
+			'brandUrl' => Yii::$app->homeUrl,
+			'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
+		]);
+		echo Nav::widget([
+			'options' => ['class' => 'navbar-nav'],
+			'items' => [
+				['label' => 'Home', 'url' => ['/site/index']],
+				['label' => 'About', 'url' => ['/site/about']],
+				['label' => 'Contact', 'url' => ['/site/contact']],
+				Yii::$app->user->isGuest
+				? ['label' => 'Login', 'url' => ['/site/login']]
+				: '<li class="nav-item">'
+				. Html::beginForm(['/site/logout'])
+				. Html::submitButton(
+					'Logout (' . Yii::$app->user->identity->username . ')',
+					['class' => 'nav-link btn btn-link logout']
+				)
+				. Html::endForm()
+				. '</li>',
+				['label' => 'Calculator', 'url' => ['/site/calculator']]
+			]
+		]);
+		NavBar::end();
+		?>
+	</header>
 
-<main id="main" class="flex-shrink-0" role="main">
-    <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
+	<main id="main" class="flex-shrink-0" role="main">
+		<div class="container">
+			<?php if (!empty($this->params['breadcrumbs'])): ?>
+			<?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
+			<?php endif ?>
+			<?= Alert::widget() ?>
+			<?= $content ?>
+		</div>
+	</main>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
-        </div>
-    </div>
-</footer>
+	<footer id="footer" class="mt-auto py-3 bg-light">
+		<div class="container">
+			<div class="row text-muted">
+				<div class="col-md-6 text-center text-md-start">&copy; My Company
+					<?= date('Y') ?>
+				</div>
+				<div class="col-md-6 text-center text-md-end">
+					<?= Yii::powered() ?>
+				</div>
+			</div>
+		</div>
+	</footer>
 
-<?php $this->endBody() ?>
+	<?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>

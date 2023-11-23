b991c3c6904c1ab21e97e89352f5a8465a7c7f50
Суворов Дмитрий Александрович <cr-ovr7@efko.ru>
2023-11-24 01:26:41 +0300

    last homework without history

--- config/db.php
+++ config/db.php
@@ -1,16 +1,10 @@
 <?php
-return [
+return [	  
 	'class' => 'yii\db\Connection',
-	'dsn'=> 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
-	'username' => getenv('DB_USER'),
-	'password' => getenv('DB_PASSWORD'),
+	'dsn' => 'mysql:host=127.0.0.1;dbname=calculator',
+	'username' => 'root',
+	'password' => '',
 	'charset' => 'utf8',
-	  
-	// 'class' => 'yii\db\Connection',
-	// 'dsn' => 'mysql:host=127.0.0.1;dbname=calculator',
-	// 'username' => 'root',
-	// 'password' => '',
-	// 'charset' => 'utf8',
 
 	// Schema cache options (for production environment)
 	//'enableSchemaCache' => true,
